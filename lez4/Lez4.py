edeea08948e2a4917a76945de806cbbb7bdbeda9
elite-23 <lucacavalleri007@gmail.com>
2024-04-19 12:10:29 +0200

    lez4 aggiunto

---
+++ lez4/Lez4.py
@@ -0,0 +1,8 @@
+"""
+Let's try to define a function amed 'subtract' ourselves:
+    It should take 2 parameters;
+    Inside the function, it should subtract the two;
+    Then, return the results.
+"""
+def subtract(x,y):
+    return x-y
