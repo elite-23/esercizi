35e1c57dc99d57dcb3b2dd7bdf25dc7ef230411e
elite-23 <lucacavalleri007@gmail.com>
2024-04-22 16:28:32 +0200

    nuovi esercizi svolti

---
+++ lez4/Lez4_tris.py
@@ -0,0 +1,2 @@
+def lenght_of_last_word(s:str)-> int:
+    return len( s.split()[ len(s.split())-1 ] )
---
+++ lez4/lez4_bis.py
@@ -0,0 +1,5 @@
+x: int=121
+s=str(x)
+print(s," l'inverso è:",s[::-1])
+if s == s[::-1]:
+    print("lol")
