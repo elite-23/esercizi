2cc9199b1cc010633a0e2eabd587cf83ccc1a5e1
elite-23 <lucacavalleri007@gmail.com>
2024-05-29 14:00:52 +0200

    symmetric stuuf

--- vari/is_symmetric.py
+++ vari/is_symmetric.py
@@ -1,20 +1,30 @@
+#Luca Cavalleri
+
 def symmetric(tree: list[int]) -> bool:
     tree_dict={"0a":tree[1],"0b":tree[2]}
-    floor=2
+    floor=0
     if tree_dict["0a"] != tree_dict["0b"]:
         return False
     
+    #step=2
     for i in range(1,len(tree)//2,2):
-        if 2*(i+1)+1<len(tree):
-            for j in range(1,floor*2+1):
+        floor+=1
+        tree_dict[str(floor)+"a"]=[]
+        tree_dict[str(floor)+"b"]=[]
+        if 2*(i+1)+(floor*2)<len(tree):
+            
+            for j in range(0,floor*2):
+                #print("i=",i)
+                #print("j=",j)
+                tree_dict[str(floor)+"a"].append(tree[2*i+1+j])
+                tree_dict[str(floor)+"b"].append(tree[2*i+1+(floor*2)+j])
                 
-            #tree_dict[str(i)+"a"]=[tree[2*i+1],tree[2*(i+1)]]
-            #tree_dict[str(i)+"b"]=[tree[2*(i+1)+1],tree[2*((i+1)+1)]]
-            print(tree_dict)
-            if tree_dict[str(i)+"a"]!=list(reversed(tree_dict[str(i)+"b"])):
+            
+            if i!=1 and tree_dict[str(i-1)+"a"]!=list(reversed(tree_dict[str(i-1)+"b"])):
                 return False
-    
+    print(tree_dict)    
     return True
+
 print(symmetric([1,2,2,3,4,4,3]))
 print(symmetric([1,2,2,None,3,None,3]))
 print(symmetric([1,2,2,None,3,3,None]))
