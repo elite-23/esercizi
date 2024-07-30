5277d5ea52933f7787837ded16d04a60afb150e9
elite-23 <lucacavalleri007@gmail.com>
2024-07-30 13:48:15 +0200

    html and javascript stuff

---
+++ WEB1_FS/javascript/prova.js
@@ -0,0 +1,6 @@
+function Somma(n1, n2) {
+    return n1 + n2;
+}
+console.log("inizio programma");
+var somma = Somma(10, 20);
+console.log("la somma è" + somma);
---
+++ WEB1_FS/javascript/prova.ts
@@ -0,0 +1,8 @@
+function Somma(n1 :number, n2 :number)
+{
+    return n1+n2
+}
+
+console.log("inizio programma")
+var somma =Somma(10, 20);
+console.log("la somma è"+ somma)
