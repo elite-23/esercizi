db7a1075e906430d8b832dcf848af52bc7a938f3
elite-23 <lucacavalleri007@gmail.com>
2024-09-13 19:30:56 +0200

    minor

--- Python5-6/ServerWEBFS/server.py
+++ Python5-6/ServerWEBFS/server.py
@@ -6,9 +6,20 @@ api=Flask(__name__)
 def index():
     return render_template("index.html")
 
-
 @api.route('/pippo', methods=['GET'])
 def pippo():
     return render_template("pippo.html")
+
+@api.route('/regok', methods=['GET'])
+def regOk():
+    return render_template("reg_ok.html")
+
+@api.route('/regko', methods=['GET'])
+def regKo():
+    return render_template("reg_ko.html")
+
+@api.route('/registrati', methods=['GET'])
+def registrati():
+    return render_template("reg_ko.html")
 
 api.run(host="0.0.0.0",port=8085)
