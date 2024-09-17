5041818c47840263a7bc0fbbadeced6ac8d59d35
elite-23 <lucacavalleri007@gmail.com>
2024-09-17 14:01:44 +0200

    esercizio login e registrazione

--- Python5-6/ServerWEBFS/server.py
+++ Python5-6/ServerWEBFS/server.py
@@ -1,25 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 api=Flask(__name__)
+
+utenti: list[list[str]]=[["Mario","Password1","1","0"],["Luigi","Password2","1","0"],["Francesca","Password3","2","0"]]
+
 
 @api.route('/', methods=['GET'])
 def index():
     return render_template("index.html")
 
-@api.route('/pippo', methods=['GET'])
-def pippo():
-    return render_template("pippo.html")
-
 @api.route('/regok', methods=['GET'])
-def regOk():
-    return render_template("reg_ok.html")
+def regOk(Uname: str,Usex: str):
+    if Usex=="1":
+        Usex="un maschio"
+    else:
+        Usex="una femmina"
+    context={"Usex":Usex,"Uname":Uname}
+    return render_template("reg_ok.html", **context)
 
 @api.route('/regko', methods=['GET'])
 def regKo():
     return render_template("reg_ko.html")
 
+
+
 @api.route('/registrati', methods=['GET'])
 def registrati():
-    return render_template("reg_ko.html")
+    utente: list[str]=[str(request.args.get("fnome")),str(request.args.get("fpassword")),str(request.args.get("fsesso")),"0"]
+    print(utente)
+    if utente in utenti:
+        utenti[utenti.index(utente)][3]="1"
+        return regOk(utente[0],utente[2])
+    return regKo()
+
+@api.route("/accedi",methods=["GET"])
+def accedi():
+    utente: list[str]=[str(request.args.get("fnome")),str(request.args.get("fpassword")),"1"]
+    for u in utenti:
+        i=u
+        i.pop(2)
+        if utente == i:
+            return regOk(u[0],u[2])
+    return regKo()
+
+
 
 api.run(host="0.0.0.0",port=8085)
